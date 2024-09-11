1296f586f3dc3865d444a4b2a8725df39dff500b
Serhii Kostiuk <>
2024-09-11 08:31:50 +0200

    add new proj

---
+++ AnimalsTCA/AnimalsTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsTCAApp.swift
+//  AnimalsTCA
+//
+//  Created by Serhii Kostiuk  on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
