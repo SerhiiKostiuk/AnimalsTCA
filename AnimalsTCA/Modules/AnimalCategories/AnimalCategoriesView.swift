f37af41f2342d68f72a25fa11ea4234aa68cd81d
Serhii Kostiuk <>
2024-09-13 10:26:44 +0200

    update category store, add actions update categories view, add alerts

--- AnimalsTCA/Modules/AnimalCategories/AnimalCategoriesView.swift
+++ AnimalsTCA/Modules/AnimalCategories/AnimalCategoriesView.swift
@@ -6,13 +6,114 @@
 //
 
 import SwiftUI
+import Kingfisher
+import ComposableArchitecture
 
 struct AnimalCategoriesView: View {
+    // MARK: - Private
+    @Perception.Bindable private var store: StoreOf<AnimalCategoriesStore>
+    
+    @State private var category: AnimalEntity?
+    
+    // MARK: - Initialization
+    init(store: StoreOf<AnimalCategoriesStore>) {
+        self.store = store
+    }
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithPerceptionTracking {
+            NavigationStack(path: $store.scope(state: \.path,
+                                               action: \.categoryDetails )) {
+                GeometryReader(content: { geometry in
+                    ScrollView(.vertical) {
+                        LazyVStack(spacing: Constants.spacingM, content: {
+                            if let animalsCategories = store.animalsCategories {
+                                ForEach(animalsCategories, id: \.order) { category in
+                                    Group {
+                                        
+                                        switch category.status {
+                                        case .paid:
+                                            Button {
+                                                store.send(.didShowAddsAlertFor(category))
+                                            } label: {
+                                                CategoryCellView(category: category)
+                                            }
+                                            
+                                        case .comingSoon:
+                                            Button {
+                                                store.send(.didShowCommingSoonAlert(category.order))
+                                            } label: {
+                                                CategoryCellView(category: category)
+                                            }
+                                        default:
+                                            
+                                            NavigationLink(state: FactsStore.State(animalCategory: category)) {
+                                                CategoryCellView(category: category)
+                                            }
+                                        }
+                                    }
+                                    .frame(height: geometry.size.width / Constants.divider)
+                                }
+                            } else {
+                                ForEach(0...2, id: \.self) { _ in
+                                    CategoryCellView(category: AnimalEntity.empty)
+                                        .redacted(reason: .placeholder)
+                                }
+                                .frame(height: geometry.size.width / Constants.divider)
+                            }
+                        })
+                    }
+                })
+                .padding(.horizontal, Constants.spacingS)
+                .background {
+                    Constants.backgroundColor.ignoresSafeArea()
+                }
+            } destination: { store in
+                FactsView(store: store)
+            }
+            
+            .onAppear {
+                store.send(.start)
+            }
+            .alert($store.scope(state: \.addsAlert, action: \.addsAlert))
+            .overlay(content: {
+                if store.showAdds {
+                    VStack {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.black.opacity(Constants.opacity))
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.delay) {
+                            store.send(.addsShowed)
+                        }
+                    }
+                }
+            })
+            .alert($store.scope(state: \.commingSoonAlert, action: \.commingSoonAlert))
+        }
+    }
+}
+
+// MARK: - Constants
+
+private extension AnimalCategoriesView {
+    
+    enum Constants {
+        static let paidTitleColor = Color(red: 0.031, green: 0.227, blue: 0.921, opacity: 1)
+        static let backgroundColor = Color(red: 0.577, green: 0.358, blue: 0.749)
+        static let opacity = 0.5
+        static let delay = 2.0
+        static let divider = 3.0
+        static let spacingS = 16.0
+        static let spacingM = 20.0
     }
 }
 
 #Preview {
-    AnimalCategoriesView()
+    AnimalCategoriesView(store: .init(initialState: AnimalCategoriesStore.State(), reducer: {
+        AnimalCategoriesStore()
+    }))
 }
--- AnimalsTCA/Modules/AnimalCategories/Store/AnimalCategoriesStore.swift
+++ AnimalsTCA/Modules/AnimalCategories/Store/AnimalCategoriesStore.swift
@@ -13,38 +13,124 @@ struct AnimalCategoriesStore {
     
     private let enviroment: AnimalCategoriesEnviroment = .init(networkService: NetworkService())
     
+    @ObservableState
     struct State: Equatable {
+        @Presents var addsAlert: AlertState<Action.AddsAlert>?
+        @Presents var commingSoonAlert: AlertState<Action.CommingSoonAlert>?
+        
         var animalsCategories: [AnimalEntity]?
         var isLoading: Bool = false
         var path = StackState<FactsStore.State>()
+        var showAdds: Bool = false
+        var addsShowed: Bool = false
+        var paidCategory: AnimalEntity?
     }
     
     enum Action: Equatable {
         case start
         case categoryDetails(StackAction<FactsStore.State, FactsStore.Action>)
         case animalsResponse([AnimalEntity]?)
+        case addsAlert(PresentationAction<AddsAlert>)
+        case didShowAddsAlertFor(AnimalEntity)
+        case commingSoonAlert(PresentationAction<CommingSoonAlert>)
+        case didShowCommingSoonAlert(Int)
+        case addsShowed
+        
+        @CasePathable
+        enum AddsAlert {
+            case showAdds
+        }
+        
+        @CasePathable
+        enum CommingSoonAlert {
+            case ok
+        }
     }
     
+    // MARK: - Body
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .start:
+                state.isLoading = true
                 return .run { send in
                     let result = try? await enviroment.networkService.request(AnimalsRequest.animalsCategories,
                                                                               [AnimalAPIModel].self)
-                    let animalCategories = try result?.entities()
+                    let animalCategories = try result?.entities().sorted(by: { $0.order < $1.order })
                     await send(.animalsResponse(animalCategories))
                 }
+                
             case let .animalsResponse(animalEntites):
                 state.isLoading = false
                 state.animalsCategories = animalEntites
                 return .none
-            case let .categoryDetails(animalEntity):
+                
+            case .categoryDetails:
+                return .none
+                
+            case .addsAlert(.presented(.showAdds)):
+                state.showAdds = true
+                return .none
+                
+            case .addsAlert(.dismiss):
+                state.showAdds = false
+                return .none
+                
+            case let .didShowAddsAlertFor(entity):
+                state.paidCategory = entity
+                
+                state.addsAlert = AlertState(title: {
+                    TextState("Watch Ad to continue")
+                }, actions: {
+                    ButtonState(action: .showAdds) {
+                        TextState("Show Ad")
+
+                    }
+                    ButtonState {
+                        TextState("Cancel")
+                    }
+                })
+                
+                return .none
+                
+            case .commingSoonAlert(.presented(.ok)),
+                    .commingSoonAlert(.dismiss):
+                state.commingSoonAlert = nil
+                return .none
+                
+            case let .didShowCommingSoonAlert(order):
+                guard let category = state.animalsCategories?.first(where: { $0.order == order }) else {
+                    
+                    return .none
+                }
+                
+                state.commingSoonAlert = AlertState(title: {
+                    TextState(category.title)
+                }, actions: {
+                    ButtonState {
+                        TextState("Ok")
+                    }
+                })
+              
+                return .none
+                
+            case .addsShowed:
+                state.addsShowed.toggle()
+                state.showAdds.toggle()
+                
+                guard let paidCategory = state.paidCategory else {
+                    return .none
+                }
+                state.path.append(FactsStore.State(animalCategory: paidCategory))
+
                 return .none
             }
+        
         }
         .forEach(\.path, action: \.categoryDetails) {
             FactsStore()
         }
+        .ifLet(\.$addsAlert, action: \.addsAlert)
+        .ifLet(\.$commingSoonAlert, action: \.commingSoonAlert)
     }
 }
